620a88f5a216ebcf6e4b80d008d1b7aa956192ba
洪小春 <hongxiaochun@qdama.cn>
2023-08-31 14:39:06 +0800

    vite搭建vue3+ts,项目初始化

---
+++ .history/src/https/index_20230605163527.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { Toast } from 'vant'
+export const Service = axios.create({
+  baseURL: import.meta.env.VITE_APP_API,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+  }
+})
+export interface AxiosResponse {
+  data: any;
+  status: number;
+  statusText: string;
+}
+Service.interceptors.request.use((config:any) => {
+  // 这里可以加一些请求头参数
+  return config
+})
+Service.interceptors.response.use((response: AxiosResponse) => {
+  return new Promise((resolve, reject) =>{
+    if(response.data.code == 0) { // 这里不一定都是0，结合后端返回的code值
+      resolve(response.data.data)
+    } else {
+      Toast(response.data.message)
+      reject(response.data)
+    }
+  })
+}, (error:any) => {
+  Toast('服务器异常，请联系管理员！')
+  return Promise.reject(error)
+})
